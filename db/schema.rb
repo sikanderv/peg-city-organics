edb1db646ddb304ca9ba83f619de08d56af39c1a
Sikander <sikanderv@gmail.com>
2017-04-17 11:05:49 +0000

    Switched from SQLite3 to POSTgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,60 +12,63 @@
 
 ActiveRecord::Schema.define(version: 20170330012228) do
 
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string   "namespace"
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "active_admin_comments", id: :integer, force: :cascade do |t|
+    t.text     "namespace"
     t.text     "body"
-    t.string   "resource_id",   null: false
-    t.string   "resource_type", null: false
-    t.string   "author_type"
+    t.text     "resource_id",   null: false
+    t.text     "resource_type", null: false
+    t.text     "author_type"
     t.integer  "author_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
   end
 
-  create_table "admin_users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "admin_users", id: :integer, force: :cascade do |t|
+    t.text     "email",                  default: "", null: false
+    t.text     "encrypted_password",     default: "", null: false
+    t.text     "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.text     "current_sign_in_ip"
+    t.text     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_admin_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  create_table "attachments", force: :cascade do |t|
+  create_table "attachments", id: :integer, force: :cascade do |t|
     t.integer  "parent_id",   null: false
-    t.string   "parent_type", null: false
-    t.string   "token"
-    t.string   "file",        null: false
-    t.string   "file_name"
+    t.text     "parent_type", null: false
+    t.text     "token"
+    t.text     "file",        null: false
+    t.text     "file_name"
     t.integer  "file_size"
-    t.string   "file_type"
-    t.string   "role"
+    t.text     "file_type"
+    t.text     "role"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
+  create_table "categories", id: :integer, force: :cascade do |t|
+    t.text     "name"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string   "name"
-    t.string   "sku"
+  create_table "products", id: :integer, force: :cascade do |t|
+    t.text     "name"
+    t.text     "sku"
     t.text     "description"
     t.boolean  "active",                                default: false
     t.decimal  "weight",        precision: 8, scale: 3, default: "0.0"
@@ -75,11 +78,11 @@ ActiveRecord::Schema.define(version: 20170330012228) do
     t.boolean  "on_sale",                               default: false
     t.boolean  "featured",                              default: false
     t.boolean  "stock_control",                         default: true
-    t.string   "image"
+    t.text     "image"
     t.integer  "category_id"
     t.datetime "created_at",                                            null: false
     t.datetime "updated_at",                                            null: false
-    t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["category_id"], name: "index_products_on_category_id", using: :btree
   end
 
 end
